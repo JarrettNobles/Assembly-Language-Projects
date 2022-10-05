e1333d6748887fad400cdf4d3e671db7cf805273
nobles_jm <noblesjarrett035@gmail.com>
2022-10-05 15:57:11 -0400

    possibly finished

--- p3/summing.asm
+++ p3/summing.asm
@@ -22,13 +22,42 @@ global _main
 _main:
 
 ; put your code here.
+
+; sum of bArray
 add BH,[bArray +1]
 add BH,[bArray +2]
 add BH,[bArray +3]
 add BH,[bArray +4]
 add BH,[bArray +5]
 mov[bArraySum], BH
-movsx eax BH
+movsx eax, BH
+
+; sum of wArray
+add AH,[wArray +1]
+add AH,[wArray +2]
+add AH,[wArray +3]
+add AH,[wArray +4]
+add AH,[wArray +5]
+mov[wArraySum], AH
+movsx ebx, AH
+
+; sum of dArray
+add CH,[dArray+1]
+add CH,[dArray+2]
+add CH,[dArray+3]
+add CH,[dArray+4]
+add CH,[dArray+5]
+mov [dArraySum], CH
+movsx ecx, CH 
+
+; sum the arrays 
+add DH,[bArraySum +1]
+add DH,[wArraySum +2]
+add DH,[dArraySum +3]
+mov[grandTotal], DH
+movsx edx, DH
+
+lastBreak:
 
 ; Normal termination code
 mov eax, 1
